23dcee4e59ffa54d5a1ad9711c7277a5c1f62aa3
Jeremy Archer <open-source@fatlotus.com>
2015-08-23 00:35:24 +0900

    Add Marketplace live migration feature.

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 # Pull packages from vendor/ directory.
 import sys; sys.path.insert(0, "vendor")
 
-from state import SECRET_KEY
-
-from flask import Flask, render_template, session
+import state
+from flask import Flask, request, render_template, session
 
 app = Flask(__name__)
-app.secret_key = SECRET_KEY
+app.secret_key = state.SECRET_KEY
 
 @app.route("/")
 def home_page():
     if not 'hits' in session:
         session['hits'] = 0
     session['hits'] += 1
-    return render_template("home_page.html", name="App Engine", hits=session['hits'])
+    return render_template(
+        "home_page.html",
+        name="App Engine",
+        hits=session['hits']
+    )
+
+@app.route("/_pull")
+def pull_from_legacy_site():
+    state.pull_from_listing(request.args.get("permalink", ""))
+    return "ok"
 
 # Run a debug server if running locally.
 if __name__ == "__main__":
--- state.py
+++ state.py
@@ -1,11 +1,53 @@
-from google.appengine.ext import ndb
 import os
+import urllib2
+import json
+import re
+import datetime
+
+from google.appengine.ext import ndb
+from google.appengine.api import mail
 
 class Listing(ndb.Model):
-    posted_by = ndb.StringProperty() # an email address
-    title = ndb.StringProperty()
+    seller = ndb.StringProperty() # an email address
+    description = ndb.StringProperty()
     details = ndb.TextProperty()
+    price = ndb.IntegerProperty() # in cents of a U.S. dollar
     posted_at = ndb.DateTimeProperty() # set to None iff not yet published
+
+def pull_from_listing(permalink):
+    try:
+        # Retrieve existing listing by the permalink.
+        if not re.match(r"^[a-zA-Z\-0-9]+$", permalink):
+            raise ValueError("Invalid permalink: {!r}".format(permalink))
+        url = "http://marketplace.uchicago.edu/{}.json".format(permalink)
+        json_data = json.load(urllib2.urlopen(url))
+
+        # Parse the listing date from not quite ISO8601 to App Engine UTC.
+        posted_at = datetime.datetime.strptime(
+            json_data["renewed_at"][:-6], "%Y-%m-%dT%H:%M:%S"
+        ) - datetime.timedelta(
+            hours=float(json_data["renewed_at"][-6:-3]),
+            minutes=float(json_data["renewed_at"][-2:])
+        )
+
+        # (Idempotently) save this entity into the datastore.
+        Listing(
+            id=json_data["permalink"],
+            seller=json_data["seller"]["email"],
+            posted_at=posted_at.replace(tzinfo=None),
+            description=json_data["description"],
+            details=json_data["details"],
+            price=(int(float(json_data["price"]) * 100))
+        ).put()
+
+    except Exception:
+        mail.send_mail(
+            "noreply@hosted-caravel.appspotmail.com",
+            "open-source@fatlotus.com",
+            "Automatic Parsing Failed",
+            permalink
+        )
+        raise
 
 class SharedSecret(ndb.Model):
     value = ndb.BlobProperty()
