6ea9d7a79fc1cfd974eb7e7c54910d36a0f5cc12
Jeremy Archer <open-source@fatlotus.com>
2015-09-08 21:45:50 +0900

    Add rudimentary support for registration and inquiry emails.

---
+++ caravel/policy.py
@@ -0,0 +1,15 @@
+def is_authorized_seller(email):
+    """Returns true if the user can use Marketplace to sell things."""
+    try:
+        user, domain = email.split("@")
+        return domain == "uchicago.edu"
+    except ValueError:
+        return False
+
+def is_authorized_buyer(email):
+    """Returns true if the user can use Marketplace to sell things."""
+    try:
+        user, domain = email.split("@")
+        return bool(domain)
+    except ValueError:
+        return False
