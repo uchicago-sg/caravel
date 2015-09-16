ceaf592689fa4ca773b8de9fe69dc4f1781c6c89
Jeremy Archer <open-source@fatlotus.com>
2015-09-16 13:01:24 -0500

    Make cache return entities instead of dicts.

--- caravel/storage/cache.py
+++ caravel/storage/cache.py
@@ -2,8 +2,49 @@
 The cache uses memcached to dramatically reduce the number of database queries.
 """
 
-import functools, logging, json, os
+import functools, logging, json, os, sys
+from json import JSONEncoder, JSONDecoder
+
 from google.appengine.api import memcache
+from google.appengine.ext import db
+from google.appengine.datastore.entity_pb import EntityProto
+
+class DBEncoder(JSONEncoder):
+    """Packs an entity as a JSON record."""
+
+    def default(self, obj):
+        """Converts an object to a JSON-compatible type."""
+
+        # Pack entities as JSON Objects.
+        if isinstance(obj, db.Model):
+            record, klass = db.to_dict(obj), obj.__class__
+            record["__ent__"] = (klass.__module__, klass.__name__)
+            if obj.is_saved():
+                record["key_name"] = obj.key().name()
+            return record
+
+        return obj
+
+class DBDecoder(JSONDecoder):
+    """Unpacks an entity from a JSON record."""
+
+    def __init__(self, *vargs, **kwargs):
+        """Initialize this JSONDecoder."""
+
+        super(DBDecoder, self).__init__(
+            object_hook=self.object_hook, *vargs, **kwargs)
+
+    def object_hook(self, obj):
+        """Parse this object from JSON."""
+
+        # If we have an entity-like dict, unpack it.
+        if type(obj) is dict and "__ent__" in obj:
+            module, kind = obj["__ent__"]
+            del obj["__ent__"]
+            klass = getattr(sys.modules[module], kind)
+            return klass(**obj)
+
+        return obj
 
 def cache(function):
     """
@@ -23,7 +64,7 @@ def cache(function):
         # Retrieve from cache.
         serialized = memcache.get(key)
         if serialized:
-            result = json.loads(serialized)
+            result = DBDecoder().decode(serialized)
             if result:
                 return result
 
@@ -31,7 +72,7 @@ def cache(function):
         logging.warning("Cache miss: " + key)
         value = function(*vargs, **kwargs)
         if value:
-            memcache.set(key, json.dumps(value), time=3600) # 1 hour
+            memcache.set(key, DBEncoder().encode(value), time=3600) # 1 hour
         return value
 
     def invalidate(*vargs, **kwargs):
