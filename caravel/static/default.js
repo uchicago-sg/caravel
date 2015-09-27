bd029b885db7534094c3fad4460eb5bdf411e8cc
georgeteo <george.c.teo@gmail.com>
2015-09-27 10:41:53 -0700

    adding javascript media handler

--- caravel/static/default.js
+++ caravel/static/default.js
@@ -1,14 +1,8 @@
-var mq = window.matchMedia('all and (max-width: 700px)');
+var mq = window.matchMedia('all and (max-width: 750px)');
 if(mq.matches) {
     // the width of browser is more then 700px
 } else {
     // the width of browser is less then 700px
+    $( ".row-eq-height").remove();
 }
 
-mq.addListener(function(changed) {
-    if(changed.matches) {
-        // the width of browser is more then 700px
-    } else {
-        // the width of browser is less then 700px
-    }
-});
