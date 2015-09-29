9186c185107614741ac63425bd450c71d875db75
georgeteo <george.c.teo@gmail.com>
2015-09-29 12:08:26 -0500

    removing row-eq-height mechanic

--- caravel/static/default.js
+++
@@ -1,8 +0,0 @@
-var mq = window.matchMedia('all and (max-width: 750px)');
-if(mq.matches) {
-    // the width of browser is more then 700px
-} else {
-    // the width of browser is less then 700px
-    $( ".row-eq-height").remove();
-}
-
