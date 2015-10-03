4fa930f3dd4c3781deecf70acae1ee5fc7c41a0a
Jeremy Archer <open-source@fatlotus.com>
2015-10-03 10:16:40 -0500

    Add support for categories.

--- caravel/controllers/forms.py
+++ caravel/controllers/forms.py
@@ -3,23 +3,10 @@ from wtforms import StringField, SubmitField, TextAreaField, DecimalField
 from wtforms import FileField, FieldList, FormField, SelectMultipleField
 from wtforms.validators import Email, DataRequired, ValidationError
 from wtforms.widgets import ListWidget, CheckboxInput
-from caravel import policy, app
 from flask_wtf.csrf import CsrfProtect
 
-CATEGORIES = [
-    ("apartments", "Apartments"),
-    ("subleases", "Subleases"),
-    ("appliances", "Appliances"),
-    ("bikes", "Bikes"),
-    ("books", "Books"),
-    ("cars", "Cars"),
-    ("electronics", "Electronics"),
-    ("employment", "Employment"),
-    ("furniture", "Furniture"),
-    ("miscellaneous", "Miscellaneous"),
-    ("services", "Services"),
-    ("wanted", "Wanted"),
-]
+from caravel import policy, app
+from caravel.storage import entities
 
 class CheckboxSelectMultipleField(SelectMultipleField):
     option_widget = CheckboxInput()
@@ -44,8 +31,9 @@ class EditListingForm(Form):
     price = DecimalField("Price", places=2,
                 validators=[DataRequired()])
     description = TextAreaField("Description", validators=[DataRequired()])
-    categories = CheckboxSelectMultipleField("Categories", choices=CATEGORIES,
-                     validators=[DataRequired()])
+    categories = CheckboxSelectMultipleField("Categories",
+                    choices=entities.Listing.CATEGORIES,
+                    validators=[DataRequired()])
     photos = FieldList(FormField(ImageEntry), min_entries=5)
     submit = SubmitField("Post")
 
