32e632c7b5be4bd3a3c584d857b9737be7342291
georgeteo <george.c.teo@gmail.com>
2015-10-06 21:08:23 -0500

    moving category colors to util

--- caravel/utils.py
+++ caravel/utils.py
@@ -23,3 +23,21 @@ def as_duration(abs_time_in_seconds):
         result = "{:.0f}{}".format(math.ceil(duration / length), label)
 
     return result
+
+@app.template_filter("category_colors")
+def category_colors(category):
+    category_colors = {
+                       "apartments":"#a52a2a",
+                       "subleases": "#ff1493",
+                       "appliances": "#ff8c00",
+                       "bikes": "#6495ed",
+                       "cars": "#ff0000",
+                       "electronics": "#9370db",
+                       "employment": "#008000",
+                       "furniture": "#ff7f50",
+                       "miscelleneous": "#808080",
+                       "services": "#202baa",
+                       "wanted": "#40e0d0"
+                    }
+    return category_colors[category]
+
