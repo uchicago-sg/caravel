5a3833def4d1db1d554422c95099ce5fbf547cf3
Jeremy Archer <open-source@fatlotus.com>
2015-11-22 13:27:34 -0600

    Add email logging.

--- caravel/storage/email.py
+++ caravel/storage/email.py
@@ -3,12 +3,15 @@ Allows easy access to an SMTP server.
 """
 
 import sendgrid
+import logging
 
 from caravel.storage import config
 
 SENDER = "Marketplace Team <marketplace@lists.uchicago.edu>"
 
 def send_mail(to, subject, html, text, reply_to=None, sender=SENDER):
+    logging.debug("SendMail(to={!r}, subject={!r})".format(to, subject))
+
     email = sendgrid.Mail()
     email.set_from(sender)
     email.add_to(to)
