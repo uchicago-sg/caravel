8f7a10635dc87c27bc342b8608395b97a5575431
Jeremy Archer <open-source@fatlotus.com>
2016-01-07 13:12:24 -0600

    Workaround for Google Apps login sadness.

--- caravel/controllers/listings.py
+++ caravel/controllers/listings.py
@@ -52,7 +52,8 @@ def modify_search(add=[], remove=[]):
 
 @app.template_global()
 def login_url():
-    return users.create_login_url(request.url.encode("utf-8"))
+    return "/login"  # users.create_login_url(request.url.encode("utf-8"))
+
 
 @app.template_global()
 def logout_url():
@@ -218,10 +219,17 @@ def new_listing():
 
     return render_template("listing_form.html", form=form)
 
+
 @app.route("/about")
 def about():
     return render_template("about.html")
 
+
 @app.route("/help")
 def helppage():
     return render_template("help.html")
+
+
+@app.route("/login")
+def login_page():
+    return redirect("/")
