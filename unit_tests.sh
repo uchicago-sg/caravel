375840db7fc1d2f00e986d305283dcc542592311
Jeremy Archer <open-source@fatlotus.com>
2018-04-07 21:03:08 -0400

    Add support for Code Climate coverage

--- unit_tests.sh
+++ unit_tests.sh
@@ -8,6 +8,11 @@ else
 	source venv/bin/activate
 fi
 
+CC_TEST_REPORTER_ID=189ccc50fd525bc8cfca2bebf76da4b6051536a23746618cdab6946c14501893 ./test-reporter-latest-darwin-amd64 before-build
+
 coverage run --source=caravel $(which nose2) --log-capture \
 	--plugin nose2.plugins.doctests --with-doctest
 coverage report
+coverage xml
+
+CC_TEST_REPORTER_ID=189ccc50fd525bc8cfca2bebf76da4b6051536a23746618cdab6946c14501893 ./test-reporter-latest-darwin-amd64 after-build --exit-code 0 -t coverage.py
